e8cfbce4997fb431cf0d28f020a8e479214b9502
Vasiliy9000 <60994491+Vasiliy9000@users.noreply.github.com>
2025-04-21 17:52:15 +0300

    Create DealContactsType.php

---
+++ php_interface/plweb/lib/Plweb/UserType/DealContactsType.php
@@ -0,0 +1,10 @@
+protected static function getOwnerTypeName(): string
+    {
+        return CCrmOwnerType::ContactName;
+    }
+
+    //protected static function getOwnerType()
+    protected static function getOwnerType(): int
+    {
+        return CCrmOwnerType::Contact;
+    }
